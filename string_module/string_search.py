5513ca6185b398962ef8e16f7dd2858ba439c5a5
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2023-01-25 13:16:09 +0530

    Finished string search module

---
+++ string_module/string_search.py
@@ -0,0 +1,12 @@
+name = 'maransowthri'
+print(name.index('ri'))
+print(name.find('rii'))
+names = ['maran', 'karan']
+print(names.index('maran'))
+print(name.rfind('w'))
+print(name.isalnum())
+print(name.isalpha())
+print('123'.isdigit())
+print('AAA'.isupper())
+print('asa'.islower())
+print('  '.isspace())
