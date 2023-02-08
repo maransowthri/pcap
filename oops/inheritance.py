6c35430a394caaf27ede5af3f0bf3389e1264cef
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2023-02-08 14:34:16 +0530

    Inheriting properties module

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -1,17 +1,32 @@
 class Vehicle:
-    def __init__(self):
+    def __init__(self, speed):
+        self.speed = speed
         pass
+
+    def introduce(self):
+        print('My name is Vehicle')
 
 
 class LandVehicle(Vehicle):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, wheel_count, speed):
+        super().__init__(speed)
+        self.wheel_count = wheel_count
 
+    def introduce(self):
+        print('My name is LandVehicle')
 
 class Car(LandVehicle):
     def __init__(self):
-        super().__init__()
+        pass
 
-print(issubclass(LandVehicle, Vehicle))
-print(issubclass(Car, LandVehicle))
-print(issubclass(Car, Car))
+    # def introduce(self):
+    #     print('My name is Car')
+
+# print(issubclass(LandVehicle, Vehicle))
+# print(issubclass(Car, LandVehicle))
+# print(issubclass(Car, Car))
+
+lambo = Car()
+print(lambo.__dict__)
+mercedez = Car(4, 150)
+print(mercedez.__dict__)
